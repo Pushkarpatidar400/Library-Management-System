c7f4fc2dc0919d8f054fb3721ad7b64cbb34e6b4
pushkarpatidar400 <pushkarpatidar400@gmail.com>
2023-09-05 14:38:21 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div>
+    <strong>Library Management System | Brought To You By <a href="https://github.com/Pushkarpatidar400">Pushkar Patidar</a> 2023</strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Home Page</b>
+      </div>
+      <strong> Library Management System | Brought To You By <a href="https://github.com/Pushkarpatidar400">Pushkar-Patidar</a> in 2023</strong>
+      </div>
+    <!-- /.container -->
+</footer>
